1dba527922683144bbf0e5a227ca097c4703446a
Vinoth Kumar Asaithambi <me@geekyvin.com>
2017-03-29 22:24:36 +0100

    Added-first-exercise

---
+++ Exercise/Chapter 1.clj
@@ -0,0 +1,34 @@
+(ns paip.1
+  (:require [clojure.string :as str]))
+
+;;-------------------------------------------------------------------
+;;1.1 Define a verison of last-name...
+;;-------------------------------------------------------------------
+
+(defn suffix? [x]
+  (let [suffix-set (list '"mr" '"sr" '"md" '"jr")]
+    (some #(= x %) suffix-set)))
+
+;;(suffix? 'mr)
+
+(defn not-suffix? [x]
+  (not (suffix? x)))
+
+(defn find-last-name [name]
+   (first (filter #(not-suffix? (str %)) (reverse (re-seq #"\w+" name)))))
+
+;;(reverse (re-seq #"\w+" "Robert Downey jr"))
+
+;;(filter #(not-suffix? (str %)) ['"jr" 'Downey 'Robert])
+
+;;(not-suffix? 'jr)
+
+(find-last-name "Robert Downey jr")
+
+;;----------------------End of Exercise 1.1----------------------------------
+;; Program to Exponeniate
+;;
+;;---------------------------------------------------------------------------
+
+
+
